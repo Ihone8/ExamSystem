407b69ab2a5ca61d7d8e46ed31207502452aeb60
W <2213629594@qq.com>
2019-04-20 20:18:11 +0800

    frist commit to github

---
+++ ExamSystem/frmExam.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ExamSystem
+{
+    partial class frmExam
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.lblQuestionName = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.tableQuestions = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.rbD);
+            this.panel1.Controls.Add(this.rbC);
+            this.panel1.Controls.Add(this.rbB);
+            this.panel1.Controls.Add(this.rbA);
+            this.panel1.Controls.Add(this.lblQuestionName);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(200, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 267);
+            this.panel1.TabIndex = 0;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Font = new System.Drawing.Font("宋体", 14F);
+            this.rbD.Location = new System.Drawing.Point(17, 212);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(37, 23);
+            this.rbD.TabIndex = 1;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "D";
+            this.rbD.UseVisualStyleBackColor = true;
+            this.rbD.CheckedChanged += new System.EventHandler(this.rbD_CheckedChanged);
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Font = new System.Drawing.Font("宋体", 14F);
+            this.rbC.Location = new System.Drawing.Point(17, 164);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(37, 23);
+            this.rbC.TabIndex = 1;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "C";
+            this.rbC.UseVisualStyleBackColor = true;
+            this.rbC.CheckedChanged += new System.EventHandler(this.rbC_CheckedChanged);
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Font = new System.Drawing.Font("宋体", 14F);
+            this.rbB.Location = new System.Drawing.Point(17, 116);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(37, 23);
+            this.rbB.TabIndex = 1;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "B";
+            this.rbB.UseVisualStyleBackColor = true;
+            this.rbB.CheckedChanged += new System.EventHandler(this.rbB_CheckedChanged);
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Font = new System.Drawing.Font("宋体", 14F);
+            this.rbA.Location = new System.Drawing.Point(17, 68);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(37, 23);
+            this.rbA.TabIndex = 1;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "A";
+            this.rbA.UseVisualStyleBackColor = true;
+            this.rbA.CheckedChanged += new System.EventHandler(this.rbA_CheckedChanged);
+            // 
+            // lblQuestionName
+            // 
+            this.lblQuestionName.AutoSize = true;
+            this.lblQuestionName.Font = new System.Drawing.Font("宋体", 20F);
+            this.lblQuestionName.Location = new System.Drawing.Point(12, 9);
+            this.lblQuestionName.Name = "lblQuestionName";
+            this.lblQuestionName.Size = new System.Drawing.Size(337, 27);
+            this.lblQuestionName.TabIndex = 0;
+            this.lblQuestionName.Text = "1、计算机有那几部分组成?";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("宋体", 20F);
+            this.btnPrevious.Location = new System.Drawing.Point(200, 288);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(151, 45);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "上一题";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("宋体", 20F);
+            this.btnNext.Location = new System.Drawing.Point(399, 288);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(151, 45);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "下一题";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // tableQuestions
+            // 
+            this.tableQuestions.ColumnCount = 5;
+            this.tableQuestions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.Location = new System.Drawing.Point(1, 3);
+            this.tableQuestions.Margin = new System.Windows.Forms.Padding(2);
+            this.tableQuestions.Name = "tableQuestions";
+            this.tableQuestions.RowCount = 5;
+            this.tableQuestions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableQuestions.Size = new System.Drawing.Size(198, 264);
+            this.tableQuestions.TabIndex = 3;
+            // 
+            // frmExam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 367);
+            this.Controls.Add(this.tableQuestions);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmExam";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "咸鱼考试";
+            this.Load += new System.EventHandler(this.frmExam_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbD;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.Label lblQuestionName;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TableLayoutPanel tableQuestions;
+    }
+}
+
---
+++ ExamSystem/frmExam.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExamSystem.Services;
+using ExamSystem.Models;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ExamSystem
+{
+    public partial class frmExam : Form
+    {
+
+        QuestionService _QuestionService = new QuestionService();
+        ChoiseService _ChoiseService = new ChoiseService();
+        private RadioButton RadioButton;
+        private QUESTION _question;
+        private int Count = 0;
+        private List<QUESTION> AllQuestions;
+        private List<Button> buttons;
+        public frmExam()
+        {
+            InitializeComponent();
+            AllQuestions = _QuestionService.GetList().ToList();
+            for (int i = 0; i < AllQuestions.Count; i++)
+            {
+                Button btn = new Button();
+                btn.Height = 40;
+                btn.Text = (i + 1).ToString();
+                btn.Tag = AllQuestions[i];
+                btn.BackColor = AllQuestions[i].Choises.Where(o => o.IsCheck == 0).Count() > 0 ? Color.White : Color.Orange;
+                btn.Font = new Font(Font.FontFamily, 17);
+                btn.Click += MyButtonClick;
+                tableQuestions.Controls.Add(btn);
+            }
+        }
+
+        private void frmExam_Load(object sender, EventArgs e)
+        {
+            _question = AllQuestions[Count];
+            this.Text = $"咸鱼考试 -- {_question.Categroy.Name}";
+            BindFormData(_question);
+        }
+
+        public void BindFormData(QUESTION question)
+        {
+            lblQuestionName.Text = $"{Count + 1}、{question.Name}";
+            rbA.Text = $"A: {question.Choises[0].Name}";
+            rbA.Checked = question.Choises[0].IsCheck == 0 ? true : false;
+            rbB.Text = $"B: {question.Choises[1].Name}";
+            rbB.Checked = question.Choises[1].IsCheck == 0 ? true : false;
+            rbC.Text = $"C: {question.Choises[2].Name}";
+            rbC.Checked = question.Choises[2].IsCheck == 0 ? true : false;
+            rbD.Text = $"D: {question.Choises[3].Name}";
+            rbD.Checked = question.Choises[3].IsCheck == 0 ? true : false;
+        }
+
+        private void MyButtonClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            Count = Convert.ToInt32(btn.Text) - 1;
+            _question = btn.Tag as QUESTION;
+            if (_question.Choises.Where(o => o.IsCheck == 0).Count() > 0)
+            {
+                btn.BackColor = Color.White;
+            }
+            BindFormData(_question);
+
+        }
+        private void rbA_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeAnswer(rbA);
+        }
+        public void ChangeAnswer(RadioButton radioButton)
+        {
+            if (radioButton.Checked)
+            {
+                var chiose = _question.Choises.Where(o => o.Name == radioButton.Text.Split(':')[1].Trim()).FirstOrDefault();
+                if (chiose != null)
+                {
+                    chiose.IsCheck = 0;
+                    var list = _question.Choises.Where(o => o.Id != chiose.Id).ToList();
+                    list.ForEach(o => o.IsCheck = 1);
+                    _ChoiseService.Update(chiose);
+                    _ChoiseService.Update(list);
+                }
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            Count++;
+            if (Count >= AllQuestions.Count)
+            {
+                Count = 0;
+                _question = AllQuestions[Count];
+                BindFormData(_question);
+                //MessageBox.Show("已经最后一题呢。", "考试提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                _question = AllQuestions[Count];
+                BindFormData(_question);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (Count >= AllQuestions.Count)
+            {
+                Count = 0;
+                _question = AllQuestions[Count];
+                BindFormData(_question);
+            }
+            else
+            {
+                Count--;
+                if (Count < 0)
+                {
+                    Count = AllQuestions.Count - 1;
+                    _question = AllQuestions[Count];
+                    BindFormData(_question);
+                }
+                else
+                {
+                    _question = AllQuestions[Count];
+                    BindFormData(_question);
+                }
+            }
+        }
+        private void rbB_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeAnswer(rbB);
+        }
+
+        private void rbC_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeAnswer(rbC);
+        }
+
+        private void rbD_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeAnswer(rbD);
+        }
+
+    }
+}
